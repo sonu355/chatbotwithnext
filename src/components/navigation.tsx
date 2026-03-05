904a4294e60e0f62bee412a57e6312b5984dd609
harshal chabukswar <harshalc562@gmail.com>
2026-03-05 14:39:49 +0530

    Added navigation component and integrated it into the root layout to add the signUp and signOut

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,37 @@
+import { 
+    SignInButton, 
+    SignOutButton, 
+    SignUpButton, 
+    Show 
+} from "@clerk/nextjs";
+import { Button } from "@base-ui/react";
+
 export const Navigation = () => {
-    
+    return (
+        <nav className="border-b border-[var(--foreground)]/10">
+            <div className="flex container h-16 items-center justify-between px-4 mx-auto">
+                <div className="text-xl font-semibold">RAG Chatbot</div>
+                
+                <div className="flex gap-2">
+                    <Show when="signed-out">
+                        <SignInButton mode="modal">
+                            <Button>Sign In</Button>
+                        </SignInButton>
+                        <SignUpButton mode="modal">
+                            <Button>Sign Up</Button>
+                        </SignUpButton>
+                    </Show>
+                    <Show when="signed-in">
+                        <SignOutButton>
+                            <Button className="border border-[var(--foreground)]/20 px-4 py-2 rounded-md hover:bg-[var(--foreground)]/5 transition-colors">
+                                Sign Out
+                            </Button>
+                        </SignOutButton>
+                    </Show>
+                </div>
+            </div>
+        </nav>
+    );
 }
+
+export default Navigation;
